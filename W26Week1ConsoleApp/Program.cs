115dfd819eef587f108740713418e3f45fb944a8
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-05 15:36:37 -0500

    avg of numbers

--- W26Week1ConsoleApp/Program.cs
+++ W26Week1ConsoleApp/Program.cs
@@ -4,12 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter radius: ");
-            double r = double.Parse(Console.ReadLine());
+            //Console.Write("Enter radius: ");
+            //double r = double.Parse(Console.ReadLine());
 
-            double area = Math.PI * r * r;
+            //double area = Math.PI * r * r;
 
-            Console.WriteLine("\nArea = " + area);
+            //Console.WriteLine("\nArea = " + area);
+
+
+            int[] numbers = { 10, 20, 0, 30 };
+            int total = 0;
+            int count = numbers.Length;
+
+            for (int i = 0; i < count; i++) // off-by-one error
+            {
+                total += numbers[i];
+            }
+
+            int average = total / count;
+            Console.WriteLine("Average: " + average);
         }
     }
 }
