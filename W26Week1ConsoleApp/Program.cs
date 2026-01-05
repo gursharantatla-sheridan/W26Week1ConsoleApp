c98278362e747bab51507b3ac0bdb2ae2e0721e2
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-05 15:10:50 -0500

    Add project files.

---
+++ W26Week1ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace W26Week1ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter radius: ");
+            double r = double.Parse(Console.ReadLine());
+
+            double area = Math.PI * r * r;
+
+            Console.WriteLine("\nArea = " + area);
+        }
+    }
+}
